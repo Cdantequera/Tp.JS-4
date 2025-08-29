f156fcd1198c98825a3584d869548566ee055a15
dAnt3 <cdantequera@gmail.com>
2025-08-28 21:30:49 -0300

    arreglos

--- script.js
+++ script.js
@@ -170,10 +170,224 @@ otroRectangulo.mostrarPropiedades();
 console.log("Perímetro: " + otroRectangulo.calcularPerimetro());
 console.log("Área: " + otroRectangulo.calcularArea()); */
 
+//ejercicio 4
 
 
 
+//ejecicio 5 
+
+
+//ejercicio 6
+
+// ejercicio 7
+
+/* class AgendaContacto {
+  #contactos;
+  #listaContacto;
+  constructor(contactos) {
+    this.#contactos = contactos;
+    this.#listaContacto = 4;
+  }
+
+  get contactos() {
+    return this.#contactos;
+  }
+
+  set nuevoContacto(contacto) {
+    this.#contactos = contacto;
+  }
+
+  get listaContacto() {
+    return this.#listaContacto;
+  }
+  // [1,2] < 4
+
+  aniadirContacto(contacto) {
+    if (this.#contactos.length < this.#listaContacto) {
+      this.#contactos.push(contacto);
+    } else {
+      alert("La agenda esta llena, no se puede agregar al contacto");
+    }
+  }
+
+  existeContacto(nombre) {
+    let aux = this.#contactos.find((contacto) =>
+      contacto.nombre.includes(nombre)
+    );
+
+    if (aux) {
+      alert("El contacto existe");
+      alert(aux);
+    } else {
+      alert("El contacto no existe");
+      alert(aux);
+    }
+  }
+
+  listaContactos() {
+    for (let i = 0; i < this.#contactos.length; i++) {
+      alert(`Mostrando contacto ${i + 1}
+        Nombre: ${this.#contactos[i].nombre}
+        Telefono: ${this.#contactos[i].telefono}`);
+    }
+  }
+
+  buscarContacto(nombre) {
+    const aux = this.#contactos.filter(
+      (contacto) => contacto.nombre === nombre
+    );
+
+    if (aux) {
+      alert(`El telefono del contacto ${aux[0].nombre} es: ${aux[0].telefono}`);
+      console.log(aux);
+    } else {
+      `El contacto no existe`;
+    }
+  }
+
+  eliminarContacto(nombre) {
+    console.log(nombre);
+    const aux = this.#contactos.findIndex(
+      (contacto) => contacto.nombre === nombre
+    );
+
+    if (aux || aux === 0) {
+      this.#contactos.splice(aux, 1);
+      alert(`Contacto eliminado`);
+    } else {
+      alert(`El contacto no existe`);
+    }
+  }
+
+  agendaLlena() {
+    if (this.#contactos.length > this.#listaContacto) {
+      alert(`La agenda esta llena`);
+    } else {
+      alert(`La agenda no esta llena`);
+    }
+  }
+
+  huecosLibres() {
+    const calculo = this.#listaContacto - this.#contactos.length;
+    if (calculo > 0) {
+      alert(`La agenda tiene ${calculo} huecos libres`);
+    } else {
+      alert(`La agenda esta llena`);
+    }
+  }
+}
+
+let agenda1 = new AgendaContacto([
+  { nombre: "Ezequiel", telefono: 381746253 },
+  { nombre: "Jose", telefono: 38123456 },
+]);
+
+alert("Bienvenido a tu agenda de contactos");
+
+do {
+  let opcion =
+    +prompt(`Ingrese un numero del 1 al 7 según lo que quiere realizar en su agenda
+    1- Agregar Contacto
+    2- Verificar si Existe un Contacto
+    3- Mostrar Contacto
+    4- Buscar el teléfono de un contacto por su nombre
+    5- Eliminar un contacto
+    6- Consultar si la agenda esta llena
+    7- Consultar cuantos huecos libres tiene la agenda`);
+
+  switch (opcion) {
+    case 1:
+      agenda1.aniadirContacto({
+        nombre: prompt("Ingrese el nombre del contacto"),
+        telefono: +prompt("Ingrese el número de teléfono"),
+      });
+      break;
+    case 2:
+      let nombre = prompt("Ingrese el nombre del contacto");
+      agenda1.existeContacto(nombre);
+      break;
+    case 3:
+      agenda1.listaContactos();
+      break;
+    case 4:
+      agenda1.buscarContacto(prompt("Ingrese el nombre del contacto"));
+      break;
+    case 5:
+      agenda1.eliminarContacto(
+        prompt("Ingrese el nombre del contacto a eliminar")
+      );
+      break;
+    case 6:
+      agenda1.agendaLlena();
+      break;
+    case 7:
+      agenda1.huecosLibres();
+      break;
+    default:
+      break;
+  }
+} while (
+  confirm("¿Desea seguir modificando su agenda o elegir una opción válida?")
+); */
 
 
 
+//Ejercicio 10
 
+class Aeropuerto{
+  constructor(nombreAeropuerto){
+    this.nombreAeropuerto = nombreAeropuerto;
+    this.listaaviones = [];
+  }
+
+  agregarAvion(avion){
+    this.listaaviones.push(avion);
+  }
+  buscaAviones(nombre){
+    const avion = this.listaaviones.find((avion) => avion.nombre === nombre);
+    if(avion){
+      console.log (`nombre: ${avion.nombre}
+      capacidad: ${avion.capacidad}
+      destino: ${avion.destino}
+      pasajeros: ${avion.listaPasajeros.length}/${avion.capacidad}`);
+      return avion
+    }else{
+      console.log("el avión no existe");
+    }
+  }
+
+}
+
+class avion{
+  constructor(nombre, capacidad, destino){
+    this.nombre = nombre;
+    this.capacidad = capacidad;
+    this.destino = destino;
+    this.listaPasajeros = [];
+  }
+
+  abordar(pasajero){
+    if(this.listaPasajeros.length < this.capacidad){
+      this.listaPasajeros.push(pasajero);
+      console.log(`Pasajero ${pasajero} abordo del avión ${this.nombre}`);
+    }else{
+      console.log(`El avión ${this.nombre} está lleno`);
+    }
+  }
+}
+
+const aeropuerto = new Aeropuerto("Aeropuerto Internacional");
+let avion1 = new avion("Boieng 747", 3, "Nueva York");
+let avion2 = new avion("Airbus A320", 2, "Los Ángeles");
+let avion3 = new avion("Boeing 777", 4, "Londres");
+
+aeropuerto.agregarAvion(avion1);
+aeropuerto.agregarAvion(avion2);
+aeropuerto.agregarAvion(avion3);
+
+console.log(aeropuerto);
+
+let avionesEncontrados = aeropuerto.buscaAviones("Boieng 747");
+if(avionesEncontrados){
+  avionesEncontrados.abordar("Juan");
+}
