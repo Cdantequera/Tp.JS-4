c90d0606a70b193b2201254d790cc21223253fd6
dAnt3 <cdantequera@gmail.com>
2025-08-31 20:31:01 -0300

    agreagodo

--- script.js
+++ script.js
@@ -172,14 +172,183 @@ console.log("Área: " + otroRectangulo.calcularArea()); */
 
 //ejercicio 4
 
+/* class Producto {
+    constructor(codigo, nombre, precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 
+    imprimeDatos() {
+        console.log(`Código: ${this.codigo}`);
+        console.log(`Nombre: ${this.nombre}`);
+        console.log(`Precio: ${this.precio}`);
+    }
+}
 
-//ejecicio 5 
+// Crear instancias de Producto
+const productos = [];
+productos.push(new Producto(1, "Producto 1", 10.99));
+productos.push(new Producto(2, "Producto 2", 15.49));
+productos.push(new Producto(3, "Producto 3", 7.99));
+
+// Mostrar datos de los productos
+productos.forEach(producto => {
+    producto.imprimeDatos();
+    console.log("-------------------");
+}); */
+
+//ejercicio 5
+
+/* class Persona {
+  constructor(nombre, edad, sexo, peso, altura, añoNacimiento) {
+    // Listas de valores aleatorios
+    
+    const nombres = ['Daniel', 'Claudia', 'Carlos', 'Pilar', 'Alfredo', 'Aurora', 'Felipe', 'Saula'];
+    const sexos = ['H', 'M'];
+    
+    // Asignar valores aleatorios si no se proporcionan
+    this.nombre = nombre || nombres[Math.floor(Math.random() * nombres.length)];
+    this.edad = edad || Math.floor(Math.random() * 82) + 18; // Entre 18 y 100 años
+    this.DNI = this.generaDNI();
+    this.sexo = sexo || sexos[Math.floor(Math.random() * sexos.length)];
+    this.peso = peso || Math.floor(Math.random() * 80) + 50; // Entre 50 y 130 kg
+    this.altura = altura || Math.floor(Math.random() * 60) + 150; // Entre 150 y 210 cm
+    this.añoNacimiento = añoNacimiento || new Date().getFullYear() - this.edad;
+  }
+
+  mostrarGeneracion = () => {
+    let gen = '';
+    let rasgo = '';
+    
+    if (this.añoNacimiento >= 1930 && this.añoNacimiento <= 1948) {
+      gen = 'Silent Generation';
+      rasgo = 'Austeridad';
+    } else if (this.añoNacimiento >= 1949 && this.añoNacimiento <= 1968) {
+      gen = 'Baby Boomer';
+      rasgo = 'Ambición';
+    } else if (this.añoNacimiento >= 1969 && this.añoNacimiento <= 1980) {
+      gen = 'Generación X';
+      rasgo = 'Obsesión por el éxito';
+    } else if (this.añoNacimiento >= 1981 && this.añoNacimiento <= 1993) {
+      gen = 'Generación Y';
+      rasgo = 'Frustración';
+    } else if (this.añoNacimiento >= 1994 && this.añoNacimiento <= 2010) {
+      gen = 'Generación Z';
+      rasgo = 'Irreverencia';
+    } else {
+      gen = 'No clasificado';
+      rasgo = 'Sin rasgo definido';
+    }
+    
+    console.log(`Pertenece a ${gen}, Rasgo: ${rasgo}`);
+  };
+
+  esMayorDeEdad = () => 
+    this.edad >= 18 ? 'Es mayor de edad' : 'Es menor de edad';
+
+  mostrarDatos = () => 
+    `Nombre: ${this.nombre},
+    \nEdad: ${this.edad}, 
+    \nDNI: ${this.DNI}, 
+    \nSexo: ${this.sexo}, 
+    \nPeso: ${this.peso}, 
+    \nAltura: ${this.altura}, 
+    \nAño Nacimiento: ${this.añoNacimiento}`;
+
+  generaDNI = () => 
+    Math.floor(Math.random() * 100000000).toString().padStart(8, '0');
+}
+
+//  creando personas con valores aleatorios
+const personaRandom1 = new Persona();
+const personaRandom2 = new Persona();
+
+console.log("Persona 1:");
+console.log(personaRandom1.mostrarDatos());
+personaRandom1.mostrarGeneracion();
+console.log(personaRandom1.esMayorDeEdad());
+
+console.log("\nPersona 2:");
+console.log(personaRandom2.mostrarDatos());
+personaRandom2.mostrarGeneracion();
+console.log(personaRandom2.esMayorDeEdad()); */
+
 
 
 //ejercicio 6
 
-// ejercicio 7
+
+/* // Creamos la clase Libro
+class Libro {
+    // El constructor recibe los datos del libro al crearlo
+    constructor(ISBN, titulo, autor, numPaginas) {
+        this.ISBN = ISBN;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+    }
+
+    // Métodos para obtener los valores de las propiedades (getters)
+    getISBN() {
+        return this.ISBN;
+    }
+
+    getTitulo() {
+        return this.titulo;
+    }
+
+    getAutor() {
+        return this.autor;
+    }
+
+    getNumPaginas() {
+        return this.numPaginas;
+    }
+
+    // Métodos para cambiar los valores de las propiedades (setters)
+    setISBN(nuevoISBN) {
+        this.ISBN = nuevoISBN;
+    }
+
+    setTitulo(nuevoTitulo) {
+        this.titulo = nuevoTitulo;
+    }
+
+    setAutor(nuevoAutor) {
+        this.autor = nuevoAutor;
+    }
+
+    setNumPaginas(nuevoNumPaginas) {
+        this.numPaginas = nuevoNumPaginas;
+    }
+
+    // Método para mostrar la información completa del libro
+    mostrarLibro() {
+        console.log(`El libro ${this.titulo} con ISBN ${this.ISBN} creado por el autor ${this.autor} tiene ${this.numPaginas} páginas.`);
+    }
+}
+
+// Creamos los dos objetos de libro
+const libro1 = new Libro("978-84-204-1211-1", "Don Quijote de la Mancha", "Miguel de Cervantes", 1040);
+const libro2 = new Libro("978-84-484-4648-5", "La Odisea", "Homero", 264);
+
+// Mostramos la información de cada libro
+libro1.mostrarLibro();
+libro2.mostrarLibro();
+
+console.log("Comparación de páginas");
+
+// Comparamos el número de páginas y mostramos el resultado
+if (libro1.getNumPaginas() > libro2.getNumPaginas()) {
+    console.log(`El libro "${libro1.getTitulo()}" tiene más páginas.`);
+} else if (libro2.getNumPaginas() > libro1.getNumPaginas()) {
+    console.log(`El libro "${libro2.getTitulo()}" tiene más páginas.`);
+} else {
+    console.log("Ambos libros tienen la misma cantidad de páginas.");
+} */
+
+//ejercicio 7
 
 /* class AgendaContacto {
   #contactos;
@@ -330,11 +499,97 @@ do {
   confirm("¿Desea seguir modificando su agenda o elegir una opción válida?")
 ); */
 
+//ejercicio 8
+
+/* class Persona {
+    constructor(nombre, edad, profesion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.profesion = profesion;
+    }
+
+    // Método para que la persona salude.
+    saludar() {
+        console.log(`Hola, mi nombre es ${this.nombre}, tengo ${this.edad} años y soy ${this.profesion}.`);
+    }
+
+    // Método para que la persona se despida.
+    despedirse() {
+        console.log(`Adios, fue un gusto conocerte`);
+    }
+}
+
+// Creamos el primer objeto (persona1).
+const persona1 = new Persona("Claudia", 40, "Profesora");
+
+// Creamos el segundo objeto (persona2).
+const persona2 = new Persona("Daniel", 41, "Programador");
+
+// Hacemos que la persona1 salude y se despida.
+console.log("Persona 1");
+persona1.saludar();
+persona1.despedirse();
+
+console.log("\nPersona 2");
+// Hacemos que la persona2 salude y se despida.
+persona2.saludar();
+persona2.despedirse(); */
+
+//ejercicio 9
+
+/* class Animal {
+  constructor(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+
+  // Animal emite sonido random
+  emitirSonido() {
+    console.log("El animal emite un sonido.");
+  }
+}
+
+// Clase perro
+class Perro extends Animal {
+  constructor(nombre, edad) {
+    super(nombre, edad);
+  }
+
+  // Sobrescribimos emitirSonido para que sea específico de un perro.
+  emitirSonido() {
+    console.log("Guau guau");
+  }
+}
+
+// Clase hijo gato
+class Gato extends Animal {
+  constructor(nombre, edad) {
+    super(nombre, edad);
+  }
+
+  // Sobrescribimos emitirSonido para que sea específico de un gato.
+  emitirSonido() {
+    console.log("Miauuuuuuu");
+  }
+}
+
+// Creamos un objeto Perro 
+const miPerro = new Perro("Max", 3);
+console.log(`Mi perro se llama ${miPerro.nombre} y tiene ${miPerro.edad} años.`);
+miPerro.emitirSonido();
+
+
+// Creamos un objeto Gato
+const miGato = new Gato("Pelusa", 2);
+console.log(`Mi gato se llama ${miGato.nombre} y tiene ${miGato.edad} años.`);
+miGato.emitirSonido();
+*/
+
 
 
 //Ejercicio 10
 
-class Aeropuerto{
+/* class Aeropuerto{
   constructor(nombreAeropuerto){
     this.nombreAeropuerto = nombreAeropuerto;
     this.listaaviones = [];
@@ -390,4 +645,4 @@ console.log(aeropuerto);
 let avionesEncontrados = aeropuerto.buscaAviones("Boieng 747");
 if(avionesEncontrados){
   avionesEncontrados.abordar("Juan");
-}
+} */
